e08b7249a8ec95cf600752f8feb6f63b2d33a55c
unknown <1010454733@qq.com>
2019-06-01 17:45:34 +0800

    feat demo

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <draggable :list="dataStore">
+      <span slot="draggableItem" slot-scope="item" >
+        {{ item.label.text }}
+      </span>
+    </draggable>
+  </div>
+</template>
+
+<script>
+import Draggable from "./components/draggable";
+export default {
+  name: 'app',
+  components: {
+    Draggable
+  },
+  data() {
+    return {
+      dataStore: [
+        { text: "name", id: 1 },
+        { text: "label", id: 2 },
+        { text: "war", id: 3 },
+        { text: "lol", id: 4 },
+        { text: "game", id: 5 },
+        { text: "basketball", id: 6 },
+        { text: "football", id: 7 },
+        { text: "run", id: 8 },
+        { text: "skip", id: 9 },
+        { text: "foot", id: 10 },
+        { text: "tail", id: 11 },
+        { text: "shallow", id: 12 },
+        { text: "border", id: 13 },
+        { text: "style", id: 14 },
+        { text: "font", id: 15 },
+        { text: "font-size", id: 16 },
+        { text: "font-family", id: 17 },
+        { text: "font-weight", id: 18 },
+        { text: "click", id: 19 },
+        { text: "input", id: 20 },
+        { text: "mouse", id: 21 },
+        { text: "cat", id: 22 },
+        { text: "banana", id: 23 },
+        { text: "pear", id: 24 }
+      ]
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/draggable.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="draggable" ref="draggable">
+        <div
+            class="label"
+            v-for="label in list"
+            :key="label.id"
+            :draggable-id="label.id"
+            :draggable="true"
+            @dragstart="dragstart"
+            @dragover="dragover"
+            @drop="drop"
+        >
+            <slot name="draggableItem" :label="label"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "draggable",
+        props: {
+            list: {
+                type: Array,
+                required: true
+            }
+        },
+        data() {
+            return {
+                dragItem: null,
+                dropItem: null
+            };
+        },
+        methods: {
+            dragstart($e) {
+                const id = $e.target.getAttribute("draggable-id");
+                const item = this.list.filter(label => label.id + "" === id)[0];
+                if (item) {
+                    this.dragItem = item;
+                }
+            },
+            dragover($e) {
+                $e.preventDefault();
+            },
+            fixDropItem(node) {
+                const name = node.getAttribute("draggable-id");
+                if (node !== document.body && name) {
+                    return name;
+                }
+                return this.fixDropItem(node.parentNode);
+            },
+            drop($e) {
+                const list = this.list;
+                const id = this.fixDropItem($e.target);
+                const item = list.filter(label => label.id + "" === id)[0];
+                if (item) {
+                    this.dropItem = item;
+                }
+                if (this.dragItem && this.dropItem && this.dragItem !== this.dropItem) {
+                    const dragIndex = list.indexOf(this.dragItem),
+                        dropIndex = list.indexOf(this.dropItem);
+                    if (~dragIndex && ~dropIndex) {
+                        list.splice(dropIndex + 1, 0, this.dragItem);
+                        list.splice(dragIndex, 1);
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.draggable {
+    width: 50%;
+    height: 300px;
+    margin: 100px auto;
+    border: 1px solid #000;
+    overflow-x: hidden;
+    overflow-y: auto;
+    border-radius: 2px;
+}
+    .label {
+        display: inline-block;
+        line-height: 28px;
+        font-size: 14px;
+        padding: 0 10px;
+        margin: 5px 5px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+</style>
